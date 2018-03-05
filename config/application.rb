e1d9f530afac404e42eeb0f1c278ec24ee161de0
EC2 Default User <ec2-user@ip-172-31-22-136.us-east-2.compute.internal>
2018-03-05 04:01:44 +0000

    Added all the templates views in views folder, and CSS, JS, IMGS, fonts in the assets folder

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,9 @@ module BerryDeliCakes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    #Fonts assets (added 03/04/2018)
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    
   end
 end
