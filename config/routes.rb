2c712027fc1186ac9875c36e14b121b1ba5d9b00
EC2 Default User <ec2-user@ip-172-31-22-136.us-east-2.compute.internal>
2018-04-06 20:22:46 +0000

    Half way session

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   resources :users
   
   get "/pages/:page" => "pages#show"
+  post   '/login'   => 'sessions#create'
+  delete '/logout'  => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
