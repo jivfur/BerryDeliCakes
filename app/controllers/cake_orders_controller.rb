7ab8ad9a4961a3193ecb4c1da7c0fe195ddae935
EC2 Default User <ec2-user@ip-172-31-22-136.us-east-2.compute.internal>
2018-04-09 06:21:09 +0000

    Error at the moment of saving Order, cake and cake price is correct

--- app/controllers/cake_orders_controller.rb
+++ app/controllers/cake_orders_controller.rb
@@ -1,4 +1,5 @@
 class CakeOrdersController < ApplicationController
+    skip_before_action :verify_authenticity_token
     
     def new
         @flavors = Flavor.all
@@ -7,21 +8,67 @@ class CakeOrdersController < ApplicationController
         
     end
     
+    
     def create
         #remember you need user Id for Orders
         #i need to send parameters that i need
+        #I need flavors
+        @flavor = Flavor.find_by_id(cake_order_params[:flavor_id])
         #save the cake..
-        @cake = Cake.new(:flavor_id, :decorationImgURL, :comments, :levels, :gallery,:cake_id, :size, :price)
-        #save the price...at this time it is only size...
-        @cake_price = Cake_Price.new(:size => cake_order_params[:size])
-        #save the order and status is ordered....
-        @order = Order.new
-        
+        cakes_params = Hash.new
+        cakes_params[:flavor_id]=@flavor.id
+        cakes_params[:decorationImgURL]=cake_order_params[:decorationImgURL]
+        cakes_params[:comments]=cake_order_params[:comments]
+        cakes_params[:levels]=cake_order_params[:levels]
+        cakes_params[:gallery]=false
+        @cake = Cake.new(cakes_params)
+        if @cake.save
+            #@cake = Cake.find_by_decorationImgURL(@cake.decorationImgURL)
+            logger.debug "Cake Id Is"+@cake.id.to_s
+            #save the price...at this time it is only size...
+            cake_price_params = Hash.new
+            cake_price_params[:cake_id] = @cake.id
+            cake_price_params[:size] = cake_order_params[:size]
+            cake_price = CakePrice.new(cake_price_params)
+            if cake_price.save
+                #save the order and status is ordered....
+                #I need a user...I was thinking that maybe there wont be necessary to have user
+                logger.debug "Cake price "+cake_price.id.to_s
+                #@cake = CakePrice.find_by_cake_id(@cake.id)
+                logger.debug "Cake price "+cake_price.id.to_s
+                order_params = Hash.new
+                user = User.find_by_id(7);
+                order_params[:user_id]=user.id
+                order_params[:orderDate]=Time.now
+                order_params[:deliveryDate]=cake_order_params[:deliveryDate]
+                order_params[:deliveryAddress]=cake_order_params[:deliveryAddress]
+                order_params[:deliveryPhone]=cake_order_params[:deliveryPhone]
+                order_params[:status]=1
+                order_params[:comments]=cake_order_params[:comments]
+                order_params[:cakePrice]=cake_price.id
+                order_params[:paidStatus]=0
+                logger.debug Order.instance_methods
+                @order = Order.new(order_params)
+                logger.debug @order.orderDate
+                logger.debug order_params
+                if @order.valid?
+                    @order.save
+                    logger.debug @order
+                    redirect_to orders_path(@order)
+                else
+                    logger.debug @order.errors.details
+                end
+            else
+                logger.debug "Cake Price was not saved it"
+            end
+        else
+            logger.debug "It was not saved it"
+        end
     end
     
     private
         # Never trust parameters from the scary internet, only allow the white list through.
         def cake_order_params
-            params.require(:order).permit(:user_id, :deliveryDate, :deliveryAddress, :deliveryPhone, :status, :comments, :cakePrice_id, :paidStatus,:flavor_id, :decorationImgURL, :comments, :levels, :gallery,:cake_id, :size, :price)
+            params.require(:order).permit(:deliveryDate, :deliveryAddress, :deliveryPhone, :comments,:flavor_id, :decorationImgURL, :comments, :levels,:size)
         end
 end
