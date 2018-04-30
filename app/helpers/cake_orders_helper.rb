e6a768bacedadd9c71919985e3b0109c44c58886
EC2 Default User <jivfur@gmail.com>
2018-04-30 06:30:28 +0000

    changes

--- app/helpers/cake_orders_helper.rb
+++ app/helpers/cake_orders_helper.rb
@@ -4,7 +4,7 @@ module CakeOrdersHelper
         #1 Half Paid
         #2 Full Paid
         #3 Refund
-        paidStatusDic = ["No Paid", "Half Paid", "Full Paid", "Refund"]
+        ["No Paid", "Half Paid", "Full Paid", "Refund"]
     end
     
     def orderStatusMethod
@@ -13,7 +13,12 @@ module CakeOrdersHelper
         #2 In the oven
         #3 Delivered
         #4 Canceled
-        orderStatusDic = ["Submitted", "Confirmed", "In the Oven", "Delivered","Canceled"]
+        ["Submitted", "Confirmed", "In the Oven", "Delivered","Canceled"]
+    end
+    
+    
+    def cakeSizeMethod
+        {6=>"4 inches - 6 people",10=>"6 inches - 10 people",28=>"8 inches - 28 people",42=>"10 inches - 42 people",56=>"12 inches - 56 people"}
     end
 
 end
