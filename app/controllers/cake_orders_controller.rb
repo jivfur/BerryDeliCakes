f01725d11ba449031fe42fa00e29e4ca0e54d7d4
EC2 Default User <jivfur@gmail.com>
2018-05-02 00:54:59 +0000

    error in cake_order

--- app/controllers/cake_orders_controller.rb
+++ app/controllers/cake_orders_controller.rb
@@ -172,9 +172,9 @@ class CakeOrdersController < ApplicationController
                     logger.debug @order.errors.full_messages
                 end
             else
-                    logger.debug "Cake Price Errors "
-                    logger.debug @cake_price.errors.full_messages
-                end
+                logger.debug "Cake Price Errors "
+                logger.debug @cake_price.errors.full_messages
+            end
             else
                 logger.debug "Cake Errors: "
                 logger.debug @cake.errors.full_messages
@@ -182,58 +182,58 @@ class CakeOrdersController < ApplicationController
         end
     end
     
-    def createOrder
-        Order.transaction do
-            ########
-            #@cake = Cake.find_by_decorationImgURL(@cake.decorationImgURL)
-            #save the price...at this time it is only size...
-            cake_price_params = Hash.new
-            pp cake_order_params
-            puts "This is cake ID"+cake_order_params[:cake_id].to_s
-            cake_price_params[:cake_id] = cake_order_params[:cake_id]
-            cake_price_params[:size] = cake_order_params[:size]
-            cake_price_params[:price] = 0.0
-            @cake_price = CakePrice.new(cake_price_params)
-            if @cake_price.save
-                pp @cake_price
-                #save the order and status is ordered....
-                #I need a user...I was thinking that maybe there wont be necessary to have user
-                ##@cake_price = CakePrice.find_by_cake_id(@cake.id)
-                ##pp @cake_price
-                order_params = Hash.new
-                order_params[:user_id]=session[:user_id]
-                order_params[:deliveryDate]=cake_order_params[:deliveryDate]
-                order_params[:deliveryAddress]=cake_order_params[:deliveryAddress]
-                order_params[:deliveryPhone]=cake_order_params[:deliveryPhone]
-                order_params[:status]=0
-                order_params[:comments]=cake_order_params[:comments]
-                #order_params[:cake_price_id]=@cake_price.id
-                order_params[:cake_price_id] =@cake_price.id
-                order_params[:paidStatus]=0
-                #logger.debug Order.instance_methods
-                pp order_params
+    # def createOrder
+    #     Order.transaction do
+    #         ########
+    #         #@cake = Cake.find_by_decorationImgURL(@cake.decorationImgURL)
+    #         #save the price...at this time it is only size...
+    #         cake_price_params = Hash.new
+    #         pp cake_order_params
+    #         puts "This is cake ID"+cake_order_params[:cake_id].to_s
+    #         cake_price_params[:cake_id] = cake_order_params[:cake_id]
+    #         cake_price_params[:size] = cake_order_params[:size]
+    #         cake_price_params[:price] = 0.0
+    #         @cake_price = CakePrice.new(cake_price_params)
+    #         if @cake_price.save
+    #             pp @cake_price
+    #             #save the order and status is ordered....
+    #             #I need a user...I was thinking that maybe there wont be necessary to have user
+    #             ##@cake_price = CakePrice.find_by_cake_id(@cake.id)
+    #             ##pp @cake_price
+    #             order_params = Hash.new
+    #             order_params[:user_id]=session[:user_id]
+    #             order_params[:deliveryDate]=cake_order_params[:deliveryDate]
+    #             order_params[:deliveryAddress]=cake_order_params[:deliveryAddress]
+    #             order_params[:deliveryPhone]=cake_order_params[:deliveryPhone]
+    #             order_params[:status]=0
+    #             order_params[:comments]=cake_order_params[:comments]
+    #             #order_params[:cake_price_id]=@cake_price.id
+    #             order_params[:cake_price_id] =@cake_price.id
+    #             order_params[:paidStatus]=0
+    #             #logger.debug Order.instance_methods
+    #             pp order_params
                 
-                @order = Order.new(order_params)
-                logger.debug @order
-                logger.debug order_params
-                ##pp @cakePrice
-                @order.cake_price_id=@cake_price.id
-                if @order.save
-                    flash[:notice] = "Your Cake is on its way!!!"
-                    redirect_to cake_order_path(@order.id)
-                else
+    #             @order = Order.new(order_params)
+    #             logger.debug @order
+    #             logger.debug order_params
+    #             ##pp @cakePrice
+    #             @order.cake_price_id=@cake_price.id
+    #             if @order.save
+    #                 flash[:notice] = "Your Cake is on its way!!!"
+    #                 redirect_to cake_order_path(@order.id)
+    #             else
                     
-                    logger.debug "Order errors "
-                    pp @order
-                    pp @cake_price
-                    logger.debug @order.errors.full_messages
-                end
-            else
-                    logger.debug "Cake Price Errors "
-                    logger.debug @cake_price.errors.full_messages
-            end
-        end
-    end
+    #                 logger.debug "Order errors "
+    #                 pp @order
+    #                 pp @cake_price
+    #                 logger.debug @order.errors.full_messages
+    #             end
+    #         else
+    #                 logger.debug "Cake Price Errors "
+    #                 logger.debug @cake_price.errors.full_messages
+    #         end
+    #     end
+    # end
     
     def destroy
         @order = Order.find(params[:id])
